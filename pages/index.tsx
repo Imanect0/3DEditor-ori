08531e2c03fb5b653fb2774e8f71b2f44d7b1cd3
yuto <gausslaw926@gmail.com>
2023-03-09 11:35:40 +0900

    @react@react-three/fiber@7.0.6 @react-three/drei@7.5.1 をインストール　→　ホームに表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,48 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useState, useEffect, useRef }  from 'react';
-import * as THREE from 'three/src/Three';
-import styles from '../styles/Home.module.css'
-import Button from '@material-ui/core/Button';
+import { NextPage } from 'next';
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
 
-import Tab from "./feature/tab";
+type BoxProps = {
+  position: [x: number, y: number, z: number];
+};
+
+const Box: React.FC<BoxProps> = (props) => {
+  const mesh = useRef<Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame(() => (mesh.current.rotation.x += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+};
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Tab />
-    </div>
+  {/* 画面いっぱいにCanvasが表示されるようdivでラップしている */}
+  return(
+    <div style={{ width: '100vw', height: '100vh' }}>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  </div>
   )
+  
 }
 
-export default Home
+
+
+export default Home;
