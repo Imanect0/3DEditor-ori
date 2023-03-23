e66cf63dc5df2da42c067e3563d0b756308108b9
Iwbchi <hfffdde2004@gmail.com>
2023-03-23 22:12:46 +0900

    add import3dObject

---
+++ pages/editor/Loader.ts
@@ -0,0 +1,111 @@
+import * as THREE from "three";
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+class MeshLoader {
+  loadingManager: THREE.LoadingManager;
+
+  constructor() {
+    this.loadingManager = new THREE.LoadingManager();
+    // ロード開始時の処理
+    this.loadingManager.onStart = (url, itemsLoaded, itemsTotal) => {
+      console.log('Started loading file');
+    }
+
+    // ロード進行中の処理
+    this.loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+      console.log('Loading file');
+    }
+
+    // ロード完了時の処理
+    this.loadingManager.onLoad = () => {
+      console.log('Loading complete!');
+    }
+
+    // ロードエラー時の処理
+    this.loadingManager.onError = (url) => {
+      console.log('There was an error loading ' + url);
+    };
+  }
+
+  // ファイル名から拡張子を取得する関数
+  getExtension(filename: string) {
+    const extension = filename.split('.').pop()?.toLowerCase();
+    return extension !== undefined ? extension : ''
+  }
+
+
+  async convert2DataUrl(inputFile: File) {
+    const reader = new FileReader()
+    reader.readAsDataURL(inputFile)
+    await new Promise<void>(resolve => reader.onload = () => resolve())
+    return reader.result 
+  }
+  // 拡張子によってloaderと成功時の処理を選択する関数
+
+  async loadFile(inputFile: File) {
+    console.log('loadFile function')
+    const fileName: string = inputFile.name
+    const reader = new FileReader();
+    let loadedModel: THREE.Object3D;
+
+    const dataUrl = await this.convert2DataUrl(inputFile)
+
+    await new Promise<void>(resolve => {
+      if (typeof dataUrl === 'string'){
+        switch (this.getExtension(fileName)) {
+  
+          case "obj":
+            new OBJLoader(this.loadingManager).load(dataUrl,
+              // success
+              (object) => {
+                loadedModel = object;
+                resolve()
+              },
+              // between loading
+              (xhr: ProgressEvent) => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
+              // called when loading has errors
+              (err: ErrorEvent) => console.log('An error happened', err)
+            );
+            break
+    
+          case "gltf":
+          case "glb":
+            new GLTFLoader(this.loadingManager).load(dataUrl,
+              // success
+              (object: GLTF) => {
+                loadedModel = object.scene;
+                resolve()
+              },
+              // between loading
+              (xhr: ProgressEvent) => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
+              // called when loading has errors
+              (err: ErrorEvent) => console.log('An error happened', err)
+            );
+            break
+    
+          case "fbx":
+            new FBXLoader(this.loadingManager).load(dataUrl,
+              // success
+              (object: THREE.Group) => {
+                loadedModel = object;
+                resolve()
+              },
+              // between loading
+              (xhr: ProgressEvent) => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
+              // called when loading has errors
+              (err: ErrorEvent) => console.log('An error happened', err)
+            );
+            break
+        }
+      }
+    })
+
+    return loadedModel;
+  }
+
+}
+export default MeshLoader
--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,16 +1,17 @@
 import { NextPage } from 'next';
 import { useEffect, useRef, useState, ReactElement } from 'react';
-import { Canvas, ThreeElements, useFrame, MeshProps } from '@react-three/fiber';
-import { DstAlphaFactor, Mesh } from 'three';
+import { Canvas, ThreeElements, useFrame, MeshProps, useLoader } from '@react-three/fiber';
 import * as THREE from "three";
 import { OrbitControls } from '@react-three/drei';
+import MeshLoader from './Loader'
+import { Object3D } from 'three';
 
 type BoxProps = {
   position: [x: number, y: number, z: number];
 };
 
 const Box: React.FC<BoxProps> = (props) => {
-  const mesh = useRef<Mesh>(null!);
+  const mesh = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   useFrame(() => (mesh.current.rotation.x += 0.01));
@@ -29,50 +30,67 @@ const Box: React.FC<BoxProps> = (props) => {
     </mesh>
   );
 };
-let isClicked : boolean;
+let isClicked: boolean;
 
-const Click = () =>{
-  
+const Click = () => {
+
 }
 
 
-const openEditMenu = () =>{
-  if(isClicked){
-    return(
-      <div style={{width : "100px" , height : "100px" , background : "black"}}></div>
+const openEditMenu = () => {
+  if (isClicked) {
+    return (
+      <div style={{ width: "100px", height: "100px", background: "black" }}></div>
     )
   }
-  else{
+  else {
 
   }
 }
 
 
 const Editor = () => {
+
+
+
   // 3dオブジェクト管理用配列
-  const [meshes, setMeshes] = useState<THREE.Mesh[]>([]);
+  const [objects, setObject] = useState<THREE.Object3D[]>([]);
+  const meshLoader = new MeshLoader()
 
-  const addMeshes = (mesh : THREE.Mesh) => setMeshes(prevMeshes => [...prevMeshes, mesh])
-  // 表示テストコード
-  useEffect(()=>{
-    addMeshes(new THREE.Mesh(new THREE.CircleGeometry(1, 1, 1), new THREE.MeshStandardMaterial({color: 'red'}))),
-    addMeshes(new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshStandardMaterial({color: 'blue'})))
-    }, [])
+  const import3dObject = async (loadedFile: File) => {
+    const object =  await meshLoader.loadFile(loadedFile);
+    addObject(object)
+  }
 
-  return(
-    <div style={{ width: '100vw', height: '100vh' }}>
-    <Canvas id='myCanvas'>
-      <OrbitControls />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      {meshes.map((mesh, index) => 
-        <mesh key={index} geometry={mesh.geometry} material={mesh.material} position={mesh.position} />
-      )}
-    </Canvas>
-  </div>
+  const addObject = (mesh: THREE.Object3D) => setObject(prevMeshes => [...prevMeshes, mesh])
+
+  // 表示テストコード(最初のみ実行)
+  // useEffect(() => {
+  //   addObject(new THREE.Mesh(new THREE.CircleGeometry(1, 1, 1), new THREE.MeshStandardMaterial({ color: 'red' }))),
+  //     addObject(new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshStandardMaterial({ color: 'blue' })))
+  // }, [])
+
+  return (
+    <>
+      <input type='file' onChange={(e) => {
+        e.target.files?.length != null ? import3dObject(e.target.files[0]): e
+      } } />
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <Canvas id='myCanvas'>
+          <OrbitControls />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          {/* <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} /> */}
+          {objects.map((mesh, index) =>
+            <primitive object={mesh} key={index}/>
+            // <mesh key={index} geometry={mesh.geometry} material={mesh.material} position={mesh.position} />
+          )}
+        </Canvas>
+      </div>
+    </>
+
   )
-  
+
 }
 export default Editor;
